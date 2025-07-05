45b742fd8adcfc6f95313b2a944729101eac56cb
Capelo <antonio.c.capelo@gmail.com>
2025-07-05 12:40:58 +0100

    feat: add validation to survey creation

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -4,10 +4,18 @@ import { QuestionBuilder } from "@/components/survey/question-builder";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { useZodFormValidation } from "@/hooks/use-zod-form-validation";
 import { saveSurvey } from "@/lib/survey";
 import { Question, Survey } from "@/types/survey";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import z from 'zod'
+
+const surveySchema = z.object({
+  title: z.string().min(1, { message: 'Title is required' }),
+  description: z.string(),
+  questions: z.array(z.any()).min(1, { message: 'At least one question is required' })
+})
 
 export default function CreateSurvey() {
   const router = useRouter();
@@ -18,8 +26,10 @@ export default function CreateSurvey() {
     questions: [],
     createdAt: new Date().toISOString(),
   });
+  const { errors, validate, resetError } = useZodFormValidation(surveySchema)
 
   const addQuestion = () => {
+    resetError('questions');
     const newQuestion: Question = {
       id: crypto.randomUUID(),
       type: "text",
@@ -50,6 +60,10 @@ export default function CreateSurvey() {
   };
 
   const handleSave = () => {
+    const isValid = validate(survey)
+    if (!isValid) {
+      return;
+    }
     saveSurvey(survey);
     router.push("/");
   };
@@ -60,11 +74,20 @@ export default function CreateSurvey() {
         <h1 className="text-3xl font-bold mb-8">Create New Test</h1>
 
         <div className="space-y-4 mb-8">
-          <Input
-            placeholder="Test Title"
-            value={survey.title}
-            onChange={(e) => setSurvey({ ...survey, title: e.target.value })}
-          />
+          <div>
+            <Input
+              placeholder="Test Title"
+              value={survey.title}
+              onChange={(e) => {
+                setSurvey({ ...survey, title: e.target.value });
+                resetError('title');
+              }}
+            />
+            {errors?.title && <p className="text-sm text-destructive mt-2">{errors.title._errors}</p>}
+          </div>
+        </div>
+
+        <div className="space-y-4 mb-8">
           <Textarea
             placeholder="Test Description"
             value={survey.description}
@@ -91,6 +114,7 @@ export default function CreateSurvey() {
           </Button>
           <Button onClick={handleSave}>Save Test</Button>
         </div>
+        {errors?.questions && <p className="text-sm text-destructive mt-2">{errors.questions._errors}</p>}
       </div>
     </div>
   );
